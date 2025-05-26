fcd748da2dfdb0e66e314dd80ce29c203003fbc7
scoobyff <pranishwarnetha02@gmail.com>
2025-05-26 20:06:50 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+// Set CORS headers
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, OPTIONS");
+header("Access-Control-Allow-Headers: *");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(204);
+    exit();
+}
+
+$get = $_GET['get'];
+$mpdUrl = 'https://a5f60e5467fc4389b2c543a65012d87e.mediatailor.us-east-1.amazonaws.com/v1' . $get;
+
+// Set headers for fetching remote
+$mpdheads = [
+  'http' => [
+      'header' => "User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/130.0.0.0 Safari/537.36\r\n",
+      'follow_location' => 1,
+      'timeout' => 5
+  ]
+];
+$context = stream_context_create($mpdheads);
+
+// Fetch and output
+$res = @file_get_contents($mpdUrl, false, $context);
+
+if ($res === false) {
+    http_response_code(502);
+    echo "Failed to fetch resource.";
+} else {
+    // Optionally forward content-type
+    header("Content-Type: application/dash+xml");
+    echo $res;
+}
+?>
